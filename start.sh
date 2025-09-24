ed5699612b425818a592bf0cb40a2d6038a43ccc
Justin <justin.liao@outlook.com>
2025-09-24 13:26:23 +0800

    修正 ssl 設定

--- start.sh
+++ start.sh
@@ -13,13 +13,13 @@ docker compose -f docker-compose.yml down
 echo "Starting  service..."
 docker compose -f docker-compose.yml up -d
 
-docker exec -it smarm-proxy nginx -t
+docker exec -it smartm-proxy nginx -t
 
-docker exec -it smarm-proxy nginx -s reload
+docker exec -it smartm-proxy nginx -s reload
 
 echo "Service has been started."
 docker ps
 
 # 確認容器運行狀態
 echo "Testing 301 redirect..."
-curl -I https://smarmt.blockcode.com.tw/support/where-to-buy/retailers
+curl -I https://smartm.blockcode.com.tw/support/where-to-buy/retailers
